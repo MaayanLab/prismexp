5531eeac498079a2143d2bb3c1b276ab7a372d45
Alexander Lachmann <alexander.lachmann@gmail.com>
2020-11-04 12:04:19 -0500

    Update loaddata.py

--- prismx/loaddata.py
+++ prismx/loaddata.py
@@ -6,12 +6,14 @@ import prismx as px
 from typing import List
 from prismx.utils import loadJSON, getConfig, getDataPath, readGMT
 
-def loadExpression(species: str, overwrite: bool = False) -> str :
+def loadExpression(species: str, overwrite: bool = False, verbose: bool = False) -> str :
     if not path.exists(getDataPath()+species+"_matrix.h5" or overwrite):
-        print("Download ARCHS4 expression")
+        if verbose:
+            print("Download ARCHS4 expression")
         urllib.request.urlretrieve(getConfig()["ARCH4_S3_BUCKET"]+"/"+species+"_matrix.h5", getDataPath()+species+"_matrix.h5")
     else:
-        print("File cached. Reload with loadExpression(\""+species+"\", overwrite=True)")
+        if verbose:
+            print("File cached. Reload with loadExpression(\""+species+"\", overwrite=True)")
     return(getDataPath()+species+"_matrix.h5")
 
 def listLibraries():
@@ -19,10 +21,12 @@ def listLibraries():
 
 def loadLibrary(library: str, overwrite: bool = False, verbose: bool = False) -> str:
     if not path.exists(getDataPath()+library or overwrite):
-        print("Download Enrichr geneset library")
+        if verbose:
+            print("Download Enrichr geneset library")
         urllib.request.urlretrieve(getConfig()["LIBRARY_DOWNLOAD_URL"]+library, getDataPath()+library)
     else:
-        print("File cached. To reload use loadLibrary(\""+library+"\", overwrite=True) instead.")
+        if verbose:
+            print("File cached. To reload use loadLibrary(\""+library+"\", overwrite=True) instead.")
     lib, rlib, ugenes = readGMT(getDataPath()+library)
     if verbose:
         print("# genesets: "+str(len(lib))+"\n# unique genes: "+str(len(ugenes)))
