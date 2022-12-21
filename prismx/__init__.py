f841944fef39ce02c3ac69ea6594fec45f3c3cb3
Alexander Lachmann <32603869+lachmann12@users.noreply.github.com>
2022-12-21 16:06:58 -0500

    fix parameter names

--- prismx/__init__.py
+++ prismx/__init__.py
@@ -19,7 +19,7 @@ from prismx.prediction import predict, prismx_predictions
 from prismx.validation import benchmark_gmt, benchmarkGMTfast, benchmark_gmt_fast
 from prismx.bridgegsea import bridge_gsea, plot_enrichment, plot_gsea, nes
 
-def create_correlation_matrices(h5file: str, outputFolder: str, clusterCount: int=50, readThreshold: int=20, sampleThreshold: float=0.01, filterSamples: int=2000, correlationMatrixCount: int=50, clusterGeneCount: int=1000, sampleCount: int=5000, reuseClustering: bool=False, correlationMethod: str="pearson", verbose: bool=True):
+def create_correlation_matrices(h5_file: str, work_dir: str, cluster_count: int=50, read_threshold: int=20, sample_threshold: float=0.01, filter_samples: int=2000, correlation_matrix_count: int=50, cluster_method: str="minibatch", cluster_gene_count: int=1000, sample_count: int=5000, reuse_clustering: bool=False, correlation_method: str="pearson", verbose: bool=True):
     '''
     Write a set of correlation matrices, by partitioning gene expression into clusters and applying Pearson
     correlation for pairs of genes. It will also create an additional matrix for global correlation.
@@ -33,30 +33,30 @@ def create_correlation_matrices(h5file: str, outputFolder: str, clusterCount: in
             Returns:
                     gene cluster mapping (pandas.DataFrame)
     '''
-    os.makedirs(outputFolder, exist_ok=True)
+    os.makedirs(work_dir, exist_ok=True)
     if verbose: print("1. Filter genes")
     tstart = time.time()
-    filtered_genes = filterGenes(h5file, readThreshold, sampleThreshold, filterSamples)
+    filtered_genes = filterGenes(h5_file, read_threshold, sample_threshold, filter_samples)
     elapsed = round((time.time()-tstart)/60, 2)
     if verbose: print("   -> completed in "+str(elapsed)+"min / #genes="+str(len(filtered_genes)))
     if verbose: print("2. Cluster samples")
     tstart = time.time()
-    clustering = create_clustering(h5file, outputFolder, filtered_genes, clusterGeneCount, clusterCount, reuseClustering=reuseClustering)
+    clustering = create_clustering(h5_file, work_dir, filtered_genes, cluster_gene_count, cluster_count, reuse_clustering=reuse_clustering, method=cluster_method)
     tclust = clustering.iloc[:,1]
-    tclust.to_csv(outputFolder+"/clustering.tsv", sep="\t")
+    tclust.to_csv(work_dir+"/clustering.tsv", sep="\t")
     elapsed = round((time.time()-tstart)/60,2)
     if verbose: print("   -> completed in "+str(elapsed)+"min")
-    if verbose: print("3. Calculate "+str(clusterCount)+" correlation matrices")
+    if verbose: print("3. Calculate "+str(cluster_count)+" correlation matrices")
     tstart = time.time()
-    mats = list(range(clusterCount))
+    mats = list(range(cluster_count))
     mats.append("global")
     j = 0
-    os.makedirs(outputFolder+"/correlation", exist_ok=True)
+    os.makedirs(work_dir+"/correlation", exist_ok=True)
     if verbose: bar = Bar('Processing correlation', max=len(mats))
     for i in range(0, len(mats)):
-        cor_mat = calculate_correlation(h5file, clustering, filtered_genes, clusterID=mats[i], maxSampleCount=sampleCount, method=correlationMethod)
+        cor_mat = calculate_correlation(h5_file, clustering, filtered_genes, clusterID=mats[i], max_sample_count=sample_count, method=correlation_method)
         cor_mat.columns = cor_mat.columns.astype(str)
-        cor_mat.reset_index().to_feather(outputFolder+"/correlation/correlation_"+str(mats[i])+".f")
+        cor_mat.reset_index().to_feather(work_dir+"/correlation/correlation_"+str(mats[i])+".f")
         j = j+1
         if verbose: bar.next()
     elapsed = round((time.time()-tstart)/60,2)
--- prismx/correlation.py
+++ prismx/correlation.py
@@ -13,7 +13,7 @@ from prismx.utils import quantile_normalize, normalize
 
 np.seterr(divide='ignore', invalid='ignore')
 
-def calculate_correlation(h5file: str, clustering: pd.DataFrame, geneidx: List[int], clusterID: str="global", maxSampleCount: int=2000, method: str="pearson") -> pd.DataFrame:
+def calculate_correlation(h5_file: str, clustering: pd.DataFrame, gene_idx: List[int], cluster_id: str="global", max_sample_count: int=2000, method: str="pearson") -> pd.DataFrame:
     '''
     Returns correlation matrix for specified samples
 
@@ -26,16 +26,16 @@ def calculate_correlation(h5file: str, clustering: pd.DataFrame, geneidx: List[i
                     correlation coefficients (pandas DataFrame)
                     average sample correlation
     '''
-    samples = np.array(a4.meta.get_meta_sample_field(h5file,'geo_accession'))
-    genes = np.array(a4.meta.get_meta_gene_field(h5file,'gene_symbol'))
+    samples = np.array(a4.meta.get_meta_sample_field(h5_file,'geo_accession'))
+    genes = np.array(a4.meta.get_meta_gene_field(h5_file,'gene_symbol'))
 
-    if clusterID == "global":
-        samplesidx = sorted(random.sample(range(len(samples)), min(maxSampleCount, len(samples))))
+    if cluster_id == "global":
+        samples_idx = sorted(random.sample(range(len(samples)), min(max_sample_count, len(samples))))
     else:
-        samplesidx = np.where(clustering.loc[:,"clusterID"] == int(clusterID))[0]
-        if maxSampleCount > 2: samplesidx = sorted(random.sample(set(samplesidx), min(len(samplesidx), maxSampleCount)))
+        samples_idx = np.where(clustering.loc[:,"clusterID"] == int(cluster_id))[0]
+        if max_sample_count > 2: samples_idx = sorted(random.sample(set(samples_idx), min(len(samples_idx), max_sample_count)))
     
-    exp = a4.data.index(h5file, samplesidx, gene_idx=geneidx, silent=True)
+    exp = a4.data.index(h5_file, samples_idx, gene_idx=gene_idx, silent=True)
     qq = normalize(exp, transpose=False)
     del exp
 
@@ -45,14 +45,14 @@ def calculate_correlation(h5file: str, clustering: pd.DataFrame, geneidx: List[i
         cc = np.corrcoef(qq)
     cc = np.nan_to_num(cc)
     del qq
-    correlation = pd.DataFrame(cc, index=genes[geneidx], columns=genes[geneidx], dtype=np.float16)
-    correlation.index = genes[geneidx]
-    correlation.columns = genes[geneidx]
+    correlation = pd.DataFrame(cc, index=genes[gene_idx], columns=genes[gene_idx], dtype=np.float16)
+    correlation.index = genes[gene_idx]
+    correlation.columns = genes[gene_idx]
     del cc
     np.fill_diagonal(correlation.to_numpy(), float('nan'))
     return correlation
 
-def create_clustering(h5file: str, workdir, geneidx: List[int], geneCount: int=1000, clusterCount: int=50, deterministic: bool=True, reuseClustering=False, method: str="minibatch") -> pd.DataFrame:
+def create_clustering(h5_file: str, work_dir, gene_idx: List[int], gene_count: int=1000, cluster_count: int=50, deterministic: bool=True, reuse_clustering=False, method: str="minibatch") -> pd.DataFrame:
     '''
     Returns cluster association for all samples in input expression h5 file
 
@@ -68,19 +68,19 @@ def create_clustering(h5file: str, workdir, geneidx: List[int], geneCount: int=1
     if deterministic:
         random.seed(42)
     
-    if reuseClustering:
+    if reuse_clustering:
         try:
-            clusterMapping = pd.read_csv(workdir+"/clustering.tsv", sep="\t")
-            clusterMapping.index = clusterMapping.iloc[:,0]
-            clusterMapping.columns=["sampleID", "clusterID"]
-            if len(set(clusterMapping.iloc[:,1])) == clusterCount:
-                return clusterMapping
+            cluster_mapping = pd.read_csv(work_dir+"/clustering.tsv", sep="\t")
+            cluster_mapping.index = cluster_mapping.iloc[:,0]
+            cluster_mapping.columns=["sampleID", "clusterID"]
+            if len(set(cluster_mapping.iloc[:,1])) == cluster_count:
+                return cluster_mapping
         except Exception:
             x = "file could not be read or clustering number does not match"
 
-    samples = a4.meta.get_meta_sample_field(h5file,'geo_accession')
+    samples = a4.meta.get_meta_sample_field(h5_file,'geo_accession')
 
-    exp = a4.data.index(h5file, list(range(len(samples))), gene_idx=sorted(random.sample(geneidx, geneCount)))
+    exp = a4.data.index(h5_file, list(range(len(samples))), gene_idx=sorted(random.sample(gene_idx, gene_count)))
 
     qq = normalize(exp, transpose=False)
     qq = pd.DataFrame(zscore(qq, axis=1)).fillna(0)
@@ -89,13 +89,13 @@ def create_clustering(h5file: str, workdir, geneidx: List[int], geneCount: int=1
     clustering = [] 
     if method == "minibatch":
         clustering = MiniBatchKMeans(init ='k-means++',
-                        n_clusters = clusterCount,
+                        n_clusters = cluster_count,
                         batch_size = 2500,
                         n_init = 10,
                         max_no_improvement = 500).fit(qq.transpose()).labels_
     else:
-        clustering = KMeans(n_clusters=clusterCount, random_state=42).fit(qq.transpose()).labels_
+        clustering = KMeans(n_clusters=cluster_count, random_state=42).fit(qq.transpose()).labels_
     qq = None     # keep memory footprint low
 
-    clusterMapping = pd.DataFrame({'sampleID': samples, 'clusterID': clustering}, index = samples, columns=["sampleID", "clusterID"])
-    return clusterMapping
+    cluster_mapping = pd.DataFrame({'sampleID': samples, 'clusterID': clustering}, index = samples, columns=["sampleID", "clusterID"])
+    return cluster_mapping
