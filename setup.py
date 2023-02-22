b18f8840479ba33d690c4f42c0fce19388177d2f
Alexander Lachmann <32603869+lachmann12@users.noreply.github.com>
2023-02-22 16:22:21 -0500

    reset validation

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="prismx",
-    version="0.2.1",
+    version="0.2.2",
     author="Alexander Lachmann",
     author_email="alexander.lachmann@mssm.edu",
     description="Package to for gene function predictions by unsupervised gene expression partitioning.",
